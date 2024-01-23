0e1124928902ccb411049e3de35caa11d4f07922
Donovan Seulin <nel640@hotmail.fr>
2024-01-23 15:28:37 +0100

    exercice 2.2

--- src/fr/ldnr/thread/TestRunnable.java
+++ src/fr/ldnr/thread/TestRunnable.java
@@ -1,20 +1,26 @@
 package fr.ldnr.thread;
-
-import javax.naming.NoPermissionException;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.joining;
 
 public class TestRunnable implements Runnable 
 {
 	@Override
 	public void run()
 	{
-		for(int i = 0 ; i < 10 ; i++)
+		for(int i = 1 ; i < 11 ; i++)
 		{
-			System.out.println(this.toString());
+			String repeated = Stream.generate(() -> "*").limit(i).collect(joining());
+			int ascii = 32 + i ;
+			String asscii = Character.toString((char) ascii);
+			System.out.println(asscii + repeated + asscii);
 		}
 	}
 	public static void main(String[] args) 
 	{
 		Thread thread = new Thread(new TestRunnable());
 		thread.start();
+		int ascii = 33;
+		String asscii = Character.toString((char) ascii);
+		//System.out.println(asscii);
 	}
 }
